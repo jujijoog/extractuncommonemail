5ad7b35164ca008c26706cc9c0129e225c0eff9e
Juji <rwkregime@gmail.com>
2018-08-16 20:15:53 -0700

    Customize for audrey

--- extract.sh
+++ extract.sh
@@ -1,3 +1,3 @@
-grep -i -s -h -r -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" insert/path/to/inputfile/here.txt > emailfilter.txt
+grep -i -s -h -r -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" Users/newjo/Mailmer > emailfilter.txt
 
-egrep -s -i -h -v "YAHOO|GMAIL|MSN|HOTMAIL" emailfilter.txt > insert/path/to/outputfile/here.txt
+egrep -s -i -h -v "YAHOO|GMAIL|MSN|HOTMAIL" emailfilter.txt > Users/newjo/Mailmer/email_list_unvalidated.txt
