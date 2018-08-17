d70b15f6d9dbd29144b163481ab4c15050c7d760
Juji <rwkregime@gmail.com>
2018-08-16 20:01:19 -0700

    Vaguerize
    "

--- extract.sh
+++ extract.sh
@@ -1,3 +1,3 @@
-grep -i -s -h -r -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" bl.txt > emailfilter.txt
+grep -i -s -h -r -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" insert/path/to/inputfile/here.txt > emailfilter.txt
 
-egrep -s -i -h -v "YAHOO|GMAIL|MSN|HOTMAIL" emailfilter.txt > email_list.txt
+egrep -s -i -h -v "YAHOO|GMAIL|MSN|HOTMAIL" emailfilter.txt > insert/path/to/outputfile/here.txt
